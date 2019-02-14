1d3804c413390b57e129de084fa21591d6d4e105
eduardo <eduardo.ximendes@4effect.com.br>
2019-02-13 22:06:59 -0200

    first commit

---
+++ server/src/main/java/com/ximendes/developer/demo/CarRepository.java
@@ -0,0 +1,9 @@
+package com.ximendes.developer.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
---
+++ server/src/main/java/com/ximendes/developer/demo/CoolCarController.java
@@ -0,0 +1,32 @@
+package com.ximendes.developer.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+public class CoolCarController {
+
+    private CarRepository  repository;
+
+    public CoolCarController(CarRepository carRepository) {
+        this.repository = carRepository;
+    }
+
+
+    @GetMapping("/cool-cars")
+    public Collection<Car> coolCars() {
+        return repository.findAll().stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !car.getName().equals("AMC Gremlin") &&
+               !car.getName().equals("Triumph Stag") &&
+               !car.getName().equals("Ford Pinto") &&
+               !car.getName().equals("Yugo GV");
+    }
+}
